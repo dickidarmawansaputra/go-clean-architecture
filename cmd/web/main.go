2f6cce2ff3d55829a34daa2676e1ecb1abff866a
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-01-26 15:28:15 +0700

    feat(cmd): added graceful shutdown

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/dickidarmawansaputra/go-clean-architecture/config"
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"syscall"
+
+	"github.com/dickidarmawansaputra/go-clean-architecture/config"
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+)
 
 func main() {
 	cfg := config.NewConfig()
@@ -12,4 +21,27 @@ func main() {
 		DB:       config.NewDatabase(cfg),
 		Validate: config.NewValidator(),
 	})
+
+	gracefulShutdown(context.Background(), app, cfg)
+}
+
+func gracefulShutdown(ctx context.Context, app *fiber.App, config *viper.Viper) {
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGHUP)
+	defer stop()
+
+	port := config.GetInt("APP_PORT")
+
+	// listen from a different goroutine
+	go func() {
+		err := app.Listen(fmt.Sprintf(":%d", port))
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	if err := app.Shutdown(); err != nil {
+		panic(err)
+	}
 }
