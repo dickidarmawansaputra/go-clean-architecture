17b166891f11bd7773512f4e15f00bd0c3011f41
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-01-28 13:41:45 +0700

    feat(controller): added auth controller register

--- internal/delivery/controller/auth_controller.go
+++ internal/delivery/controller/auth_controller.go
@@ -1,6 +1,10 @@
 package controller
 
-import "github.com/dickidarmawansaputra/go-clean-architecture/internal/usecase"
+import (
+	"github.com/dickidarmawansaputra/go-clean-architecture/internal/model"
+	"github.com/dickidarmawansaputra/go-clean-architecture/internal/usecase"
+	"github.com/gofiber/fiber/v2"
+)
 
 type AuthController struct {
 	UseCase *usecase.AuthUseCase
@@ -10,4 +14,23 @@ func NewAuthController(useCase *usecase.AuthUseCase) *AuthController {
 	return &AuthController{
 		UseCase: useCase,
 	}
+}
+
+func (c *AuthController) Register(ctx *fiber.Ctx) error {
+	request := new(model.RegisterRequest)
+
+	if err := ctx.BodyParser(request); err != nil {
+		return err
+	}
+
+	response, err := c.UseCase.Register(ctx, request)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"code":   fiber.StatusOK,
+		"status": fiber.NewError(fiber.StatusOK).Message,
+		"data":   response,
+	})
 }
