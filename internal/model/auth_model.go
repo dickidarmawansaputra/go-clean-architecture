623973c89b4bf9fc9ea9267e6a1681e8973d188e
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-01-28 13:32:09 +0700

    feat(model): added register request

---
+++ internal/model/auth_model.go
@@ -0,0 +1,7 @@
+package model
+
+type RegisterRequest struct {
+	Name     string `json:"name" validate:"required,max=100"`
+	Email    string `json:"email" validate:"required,email,max=100"`
+	Password string `json:"password" validate:"required,max=255"`
+}
