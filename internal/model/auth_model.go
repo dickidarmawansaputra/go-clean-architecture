3a4541eae7b098dcfde0d9856fdbae533aa7754e
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-01-29 01:34:30 +0700

    feat(model): added login request & auth response model

--- internal/model/auth_model.go
+++ internal/model/auth_model.go
@@ -1,7 +1,16 @@
 package model
 
+type AuthResponse struct {
+	Token string `json:"token"`
+}
+
 type RegisterRequest struct {
 	Name     string `json:"name" validate:"required,max=100"`
+	Email    string `json:"email" validate:"required,email,max=100"`
+	Password string `json:"password" validate:"required,max=255"`
+}
+
+type LoginRequest struct {
 	Email    string `json:"email" validate:"required,email,max=100"`
 	Password string `json:"password" validate:"required,max=255"`
 }
