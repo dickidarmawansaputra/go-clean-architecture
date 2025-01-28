392ded20305088c58c8ebb437608998100b9537a
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-01-28 13:40:55 +0700

    feat(usecase): added auth usecase register

--- internal/usecase/auth_usecase.go
+++ internal/usecase/auth_usecase.go
@@ -1,8 +1,14 @@
 package usecase
 
 import (
+	"fmt"
+
+	"github.com/dickidarmawansaputra/go-clean-architecture/internal/entity"
+	"github.com/dickidarmawansaputra/go-clean-architecture/internal/lib/password"
+	"github.com/dickidarmawansaputra/go-clean-architecture/internal/model"
 	"github.com/dickidarmawansaputra/go-clean-architecture/internal/repository"
 	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
 	"gorm.io/gorm"
 )
@@ -21,4 +27,42 @@ func NewAuthUseCase(config *viper.Viper, db *gorm.DB, validate *validator.Valida
 		Validate:       validate,
 		UserRepository: repository,
 	}
+}
+
+func (u *AuthUseCase) Register(ctx *fiber.Ctx, request *model.RegisterRequest) (*entity.User, error) {
+	tx := u.DB.Begin()
+	defer tx.Rollback()
+
+	if err := u.Validate.Struct(request); err != nil {
+		return nil, err
+	}
+
+	user := &entity.User{
+		Name:     request.Name,
+		Email:    request.Email,
+		Password: request.Password,
+		Photo:    fmt.Sprintf("https://ui-avatars.com/api/?name=%s", request.Name),
+	}
+
+	userExists := u.UserRepository.CheckUserExists(tx, ctx, user, request.Email)
+	if userExists {
+		return nil, fiber.NewError(fiber.StatusConflict, "User already exist")
+	}
+
+	hashedPassword, err := password.Hash(user.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	user.Password = hashedPassword
+
+	if err := u.UserRepository.Create(tx, ctx, user); err != nil {
+		return nil, err
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
