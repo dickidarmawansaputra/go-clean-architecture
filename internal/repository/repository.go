7f29f0d83b69bc55c44a1ebd35c962a7826740a1
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-01-28 13:29:53 +0700

    feat(repository): added crud command to repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,21 +1,33 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
 
 type Repository[T any] struct{}
 
-func (r *Repository[T]) Create(db *gorm.DB, entity *T) error {
-	return db.Create(entity).Error
+func (r *Repository[T]) Create(db *gorm.DB, ctx *fiber.Ctx, entity *T) error {
+	return db.WithContext(ctx.UserContext()).Create(entity).Error
 }
 
-func (r *Repository[T]) FindById(db *gorm.DB, entity *T, id uint) error {
-	return db.Where("id = ?", id).Take(entity).Error
+func (r *Repository[T]) FindById(db *gorm.DB, ctx *fiber.Ctx, entity *T, id uint) error {
+	return db.WithContext(ctx.UserContext()).Where("id = ?", id).Take(entity).Error
 }
 
-func (r *Repository[T]) Update(db *gorm.DB, entity *T, id uint) error {
-	return db.Where("id = ?", id).Save(entity).Error
+func (r *Repository[T]) Update(db *gorm.DB, ctx *fiber.Ctx, entity *T, id uint) error {
+	return db.WithContext(ctx.UserContext()).Where("id = ?", id).Save(entity).Error
 }
 
-func (r *Repository[T]) Delete(db *gorm.DB, entity *T, id uint) error {
-	return db.Where("id = ?", id).Delete(entity).Error
+func (r *Repository[T]) Delete(db *gorm.DB, ctx *fiber.Ctx, entity *T, id uint) error {
+	return db.WithContext(ctx.UserContext()).Where("id = ?", id).Delete(entity).Error
+}
+
+func (r *Repository[T]) CountByField(db *gorm.DB, ctx *fiber.Ctx, entity *T, field string, value any) (int64, error) {
+	var total int64
+	err := db.WithContext(ctx.UserContext()).Model(&entity).Where(fmt.Sprintf("%s = ?", field), value).Count(&total).Error
+
+	return total, err
 }
