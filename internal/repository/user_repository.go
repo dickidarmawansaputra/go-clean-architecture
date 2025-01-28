9b623690d892ee336f358f1c423a5c7e009e6e5e
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-01-28 13:38:32 +0700

    feat(repository): added user repository to check user exists

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,6 +1,10 @@
 package repository
 
-import "github.com/dickidarmawansaputra/go-clean-architecture/internal/entity"
+import (
+	"github.com/dickidarmawansaputra/go-clean-architecture/internal/entity"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
 
 type UserRepository struct {
 	Repository[entity.User]
@@ -8,4 +12,13 @@ type UserRepository struct {
 
 func NewUserRepository() *UserRepository {
 	return &UserRepository{}
+}
+
+func (r *UserRepository) CheckUserExists(db *gorm.DB, ctx *fiber.Ctx, entity *entity.User, email string) bool {
+	total, _ := r.CountByField(db, ctx, entity, "email", email)
+	if total > 0 {
+		return true
+	}
+
+	return false
 }
