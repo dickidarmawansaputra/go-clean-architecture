a34f48b2b1df1611da55bd5bfae0e1e5cb63a996
Dicki Darmawan Saputra <dickidarmawansaputra@gmail.com>
2025-01-28 22:33:33 +0700

    fix(config): added database logger

--- config/database.go
+++ config/database.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-func NewDatabase(config *viper.Viper) *gorm.DB {
+func NewDatabase(config *viper.Viper, log *logrus.Logger) *gorm.DB {
 	host := config.GetString("DB_HOST")
 	username := config.GetString("DB_USER")
 	password := config.GetString("DB_PASSWORD")
@@ -23,13 +25,23 @@ func NewDatabase(config *viper.Viper) *gorm.DB {
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s", host, username, password, database, port, sslmode, timezone)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.New(&loggerWriter{Logger: log}, logger.Config{
+			SlowThreshold:             time.Second * 5, // Slow SQL threshold
+			LogLevel:                  logger.Info,     // Log level
+			IgnoreRecordNotFoundError: true,            // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,            // Don't include params in the SQL log
+			Colorful:                  false,           // Disable color
+		}),
+	})
 	if err != nil {
+		log.Fatalf("failed to open connection: %v", err)
 		panic(err)
 	}
 
 	connection, err := db.DB()
 	if err != nil {
+		log.Fatalf("connection failed: %v", err)
 		panic(err)
 	}
 
@@ -38,4 +50,12 @@ func NewDatabase(config *viper.Viper) *gorm.DB {
 	connection.SetConnMaxLifetime(time.Second * time.Duration(maxLifeTimeConnection))
 
 	return db
+}
+
+type loggerWriter struct {
+	Logger *logrus.Logger
+}
+
+func (l *loggerWriter) Printf(message string, args ...interface{}) {
+	l.Logger.Tracef(message, args...)
 }
